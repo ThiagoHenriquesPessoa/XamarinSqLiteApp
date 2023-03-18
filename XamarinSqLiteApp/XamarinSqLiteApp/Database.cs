512521662bb9b789729f3596aff7dff0078c849c
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-03-18 13:24:27 -0300

    Add project files.

---
+++ XamarinSqLiteApp/XamarinSqLiteApp/Database.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinSqLiteApp
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Person>();
+        }
+
+        public Task<List<Person>> GetPeopleAsync() 
+        {
+            return _database.Table<Person>().ToListAsync(); 
+        }
+
+        public Task<int> SavePersonAsync(Person person) 
+        {
+            return _database.InsertAsync(person);
+        }
+    }
+}
---
+++ XamarinSqLiteApp/XamarinSqLiteApp/Person.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace XamarinSqLiteApp
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
